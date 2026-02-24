6f0c9ffdab403df3316c2727f1613993bc5103dd
Lam Nguyen <149017745+lamukaseedu@users.noreply.github.com>
2026-02-24 16:35:36 -0500

    implement grid system
    game instantiates a 5x5 grid when playing, player can only move within grid
    
    also moved heartbeat ui downwards

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,7 @@ public class PlayerMove : MonoBehaviour
 {
     [SerializeField] private HeartbeatUI heartbeat;
     [SerializeField] private CameraShake cameraShake;
-    [SerializeField] private GridManager gridManager;
+    [SerializeField] private GridManager grid;
     [SerializeField] private TextMeshProUGUI indicator;
     [SerializeField] private float threshold = 0.2f;
     public InputActionAsset asset;
@@ -15,7 +16,7 @@ public class PlayerMove : MonoBehaviour
     InputAction move;
     private Vector2 direction;
     private float speed;
-    private (int, int) position;
+    private (int, int) position = (0, 0);
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -46,12 +47,28 @@ public class PlayerMove : MonoBehaviour
             yield return null;
         }
         if (
-            (heartbeat.getProgress() <= threshold / 2f || heartbeat.getProgress() >= 1 - threshold / 2f)
-            && (direction.x == 0 || direction.y == 0)
-            )
+            (heartbeat.getProgress() <= threshold / 2f || heartbeat.getProgress() >= 1 - threshold / 2f))
         {
-            Vector3 target = new Vector3(transform.position.x + direction.x * 5, transform.position.y, transform.position.z + direction.y * 5);
-            transform.position = Vector3.MoveTowards(transform.position, target, 2.5f);
+            if (direction.y != 0)
+            {
+                int sign = (int) Mathf.Sign(direction.y);
+                if (grid.gridObject.CheckSpace(position.Item1, position.Item2 + 1 * sign) == 1)
+                {
+                    Vector3 target = new Vector3(position.Item1, transform.position.y, position.Item2 + 1 * sign);
+                    transform.position = Vector3.MoveTowards(transform.position, target, 2.5f);
+                    position = (position.Item1, position.Item2 + 1 * sign);
+                }
+            } else if (direction.x != 0)
+            {
+                int sign = (int)Mathf.Sign(direction.x);
+                if (grid.gridObject.CheckSpace(position.Item1 + 1 * sign, position.Item2) == 1)
+                {
+                    Vector3 target = new Vector3(position.Item1 + 1 * sign, transform.position.y, position.Item2);
+                    transform.position = Vector3.MoveTowards(transform.position, target, 2.5f);
+                    position = (position.Item1 + 1 * sign, position.Item2);
+                }
+            }
+
             if (indicator != null)
             {
                 indicator.text = "MOVE SUCCESS";
