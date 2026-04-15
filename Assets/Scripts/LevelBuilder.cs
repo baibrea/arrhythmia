05775067f7f0c0dd3facfc35a0f49f78eb71cd2f
baibrea <breannacbai@gmail.com>
2026-04-14 23:43:13 -0400

    Created room with flickering monitors, modified title screen

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -8,15 +8,34 @@ public class LevelBuilder : MonoBehaviour
     void Start()
     {
         BuildTutorial();
+        BuildLevels();
     }
 
     void BuildTutorial()
     {
         grid.FillFloor(-4, -3, 4, 3);
         grid.SetTile(-5, 0, "door");
+    }
 
+    void BuildLevels()
+    {
+        // Main Floor
         grid.FillFloor(-21, -3, -6, 3);
+
+        // Hallways
         grid.FillFloor(-19, 4, -18, 10);
+        grid.FillFloor(-14, 4, -13, 10);
+        grid.FillFloor(-9, 4, -8, 10);
+
+        // First Level
+        grid.FillFloor(-25, 11, -18, 17);
+        grid.FillFloor(-28, 15, -23, 20);
+
+        // Second Level
+        grid.FillFloor(-16, 11, -11, 20);
+
+        // Third Level
+        grid.FillFloor(-9, 11, -2, 18);
     }
 
 }
