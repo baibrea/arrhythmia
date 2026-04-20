60afe741d355741ecabb97e56381d15556486c2d
Lam Nguyen <149017745+lamukaseedu@users.noreply.github.com>
2026-04-20 17:02:44 -0400

    remove old scenes and hide walls that block player

---
+++ Assets/Scripts/CameraWall.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraWall : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private LayerMask wallMask;
+
+    private HashSet<Renderer> currentlyHidden = new HashSet<Renderer>();
+    private HashSet<Renderer> nextHidden = new HashSet<Renderer>();
+
+    void LateUpdate()
+    {
+        nextHidden.Clear();
+
+        Vector3 camPos = transform.position;
+        Vector3 playerPos = player.position;
+
+        Vector3 dir = playerPos - camPos;
+        float dist = dir.magnitude;
+
+        Ray ray = new Ray(camPos, dir.normalized);
+        RaycastHit[] hits = Physics.RaycastAll(ray, dist, wallMask);
+
+        foreach (var hit in hits)
+        {
+            Renderer r = hit.collider.GetComponent<Renderer>();
+            if (r != null)
+                nextHidden.Add(r);
+        }
+
+        foreach (var r in nextHidden)
+        {
+            if (!currentlyHidden.Contains(r))
+            {
+                SetVisible(r, false);
+            }
+        }
+        foreach (var r in currentlyHidden)
+        {
+            if (!nextHidden.Contains(r))
+            {
+                SetVisible(r, true);
+            }
+        }
+
+        var temp = currentlyHidden;
+        currentlyHidden = nextHidden;
+        nextHidden = temp;
+    }
+
+    void SetVisible(Renderer r, bool visible)
+    {
+        r.enabled = visible;
+    }
+}
