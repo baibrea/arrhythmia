fcfd5221f1983a76f37331110a5886570bfdf454
baibrea <breannacbai@gmail.com>
2026-04-21 03:37:59 -0400

    Added Final Chase Hallway

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -35,14 +35,20 @@ public class LevelBuilder : MonoBehaviour
         grid.FillWall(-5, 13, -3, 13); // Bottom wall
         grid.FillWall(-4, 16, -4, 18); // Top wall
         grid.FillFloor(-1, 17, 0, 18); // Walkway to 2nd room
+        grid.SetTile(-9, 18, "wall");
+        grid.SetTile(-6, 11, "wall");
+        grid.SetTile(-2, 11, "wall");
 
         grid.FillFloor(1, 12, 10, 22); // 2nd room floor
         grid.FillWall(3, 19, 4, 19); // 2nd room, top left wall
         grid.FillWall(7, 18, 7, 20); // 2nd room, top right wall
         grid.SetTile(8, 18, "wall");
+        grid.SetTile(1, 22, "wall");
+        grid.SetTile(10, 22, "wall");
 
         grid.FillWall(1, 15, 3, 16); // 2nd room, left wall
         grid.FillWall(6, 13, 10, 14); // 2nd room, bottom right wall
+        grid.SetTile(1, 14, "wall");
 
         grid.FillFloor(9, 8, 10, 11); // Walkway to final hallway
         grid.FillFloor(-1, 7, 10, 7); // Final hallway
@@ -55,7 +61,21 @@ public class LevelBuilder : MonoBehaviour
 
     public void BuildFinalChase()
     {
-        grid.FillFloor(-14, 4, -13, 30);
+        grid.FillFloor(-14, 4, -13, 40);
+
+        grid.SetTile(-13, 7, "wall");
+        grid.SetTile(-13, 8, "wall");
+        grid.SetTile(-13, 14, "wall");
+        grid.SetTile(-13, 22, "wall");
+        grid.SetTile(-13, 23, "wall");
+        grid.SetTile(-13, 31, "wall");
+
+        grid.SetTile(-14, 11, "wall");
+        grid.SetTile(-14, 18, "wall");
+        grid.SetTile(-14, 19, "wall");
+        grid.SetTile(-14, 27, "wall");
+        grid.SetTile(-14, 35, "wall");
+
     }
 
 }
